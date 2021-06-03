18ca3fe86894b47b962e1028fef31227340ed1f8
codeybear <30731499+codeybear@users.noreply.github.com>
2021-06-03 13:56:48 +0100

    get Collections convert to await

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 async function getUPRN(postCode) {
   const response = await fetch(`https://api.readingdev.com/rbc/getaddresses/${postCode}`);
 
@@ -12,6 +11,13 @@ async function getUPRN(postCode) {
 }
 
 function getCollections(UPRN) {
-  fetch(`https://api.readingdev.com/api/collections/${UPRN}`)
-  .then(response => response.json());
+  const response = await fetch(`https://api.readingdev.com/api/collections/${UPRN}`)
+
+  if (!response.ok) {
+    const message = `An error has occured: ${response.status}`;
+    throw new Error(message);
+  }
+
+  const json = await response.json();
+  return json
 }
