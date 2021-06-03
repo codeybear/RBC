f941a903cd075f9c9a82a7c3f67404741f42eea4
codeybear <30731499+codeybear@users.noreply.github.com>
2021-06-03 13:53:53 +0100

    First version of UPRN complete

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
-function getUPRN(postCode) {
-    fetch(`https://api.readingdev.com/rbc/getaddresses/${postCode}`)
-    .then(response => response.json())
-    .then(data => {
-      console.log('Success:', data);
-      return data;
-    })
+
+async function getUPRN(postCode) {
+  const response = await fetch(`https://api.readingdev.com/rbc/getaddresses/${postCode}`);
+
+  if (!response.ok) {
+    const message = `An error has occured: ${response.status}`;
+    throw new Error(message);
+  }
+
+  const json = await response.json();
+  return json
 }
 
 function getCollections(UPRN) {
-    fetch(`https://api.readingdev.com/api/collections/${UPRN}`)
-    .then(response => response.json())
-    .then(data => {  
-      console.log('Success:', data);
-      return data;
-    })
+  fetch(`https://api.readingdev.com/api/collections/${UPRN}`)
+  .then(response => response.json());
 }
